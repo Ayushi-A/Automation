c08f6b03da0143b205037f36f5acffe3cd559879
Ayushi <ayushics12345@gmail.com>
2021-03-02 14:21:47 +0530

    Learning GitHub

---
+++ FirstTest.java
@@ -0,0 +1,16 @@
+package Maven.MavenTest;
+
+import org.testng.annotations.Test;
+
+public class FirstTest {
+	@org.testng.annotations.Test
+	public void first()
+	{
+		System.out.println("First");
+	}
+	@Test
+	public void firstone()
+	{
+		System.out.println("Firstone");
+	}
+}
---
+++ SecondTest.java
@@ -0,0 +1,17 @@
+	package Maven.MavenTest;
+	
+	import org.testng.annotations.Test;
+	
+	public class SecondTest {
+	
+			
+			public void first()
+			{
+				System.out.println("Second");
+			}
+			@Test
+			public void firstone()
+			{
+				System.out.println("Second one");
+			}
+	}
